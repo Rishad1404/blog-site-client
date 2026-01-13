ad99bc5e6fb703258c5712843c6a0fb206c796f5
Rishad1404 <rishadislam2019@gmail.com>
2026-01-14 00:26:15 +0600

    navbar

---
+++ src/app/contact/branch/page.tsx
@@ -0,0 +1,7 @@
+export default function BranchPage(){
+    return(
+        <div>
+            <h1>This is a branch page component</h1>
+        </div>
+    );
+}
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,7 @@
+export default function ContactPage(){
+    return(
+        <div>
+            <h1>This is a contact page component</h1>
+        </div>
+    );
+}
