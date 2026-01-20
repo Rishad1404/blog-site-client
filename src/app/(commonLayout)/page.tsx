b7a4207e9a283e4f5ea62bc85e357ab4208739bf
Rishad1404 <rishadislam2019@gmail.com>
2026-01-21 00:53:21 +0600

    isfeatured in params

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -2,18 +2,19 @@ import BlogCard from "@/components/modules/homepage/BlogCard";
 import { blogService } from "@/services/blog.service";
 import { BlogPost } from "@/types";
 
-
 export default async function Home() {
+  const { data } = await blogService.getBlogPosts({
+    isFeatured: false,
+  },{
+    cache:"no-store"
 
-  const {data}=await blogService.getBlogPosts();
-
+  });
+  console.log(data);
   return (
     <div className="grid grid-cols-3 max-w-7xl mx-auto gap-5">
-      {
-        data?.data?.map((post:BlogPost)=>(
-          <BlogCard key={post.id} post={post}/>
-        ))
-      }
+      {data?.data?.map((post: BlogPost) => (
+        <BlogCard key={post.id} post={post} />
+      ))}
     </div>
   );
 }
--- src/services/blog.service.ts
+++ src/services/blog.service.ts
@@ -5,18 +5,48 @@ import { env } from "@/env";
 //* next: { revalidate: 10 } : ISR -> Mix between static and dynamic
 
 const API_URL = env.API_URL;
+
+interface ServiceOptions{
+  cache?:RequestCache
+  revalidate?:number
+}
+interface GetBlogsParams {
+  isFeatured?: boolean;
+  search?: string;
+}
+
 export const blogService = {
-  getBlogPosts: async function () {
+  getBlogPosts: async function (params?: GetBlogsParams,options?:ServiceOptions) {
     try {
-      const res = await fetch(`${API_URL}/posts`);
-      const data = await res.json();
+      const url = new URL(`${API_URL}/posts`);
+      // console.log(Object.entries(params));
 
-      // This is an example
-      // if(data.success){
-      // return
-      // }
+      if (params) {
+        Object.entries(params).forEach(([key, value]) => {
+          if (value !== undefined && value !== null && value !== "") {
+            url.searchParams.append(key, value);
+          }
+        });
 
-      return { data: data, error: null };
+        const config:RequestInit={};
+        if(options?.cache){
+          config.cache=options.cache;
+        }
+
+        if(options?.revalidate){
+          config.next={revalidate:options.revalidate}
+        }
+
+        const res = await fetch(url.toString(),config);
+        const data = await res.json();
+
+        // This is an example
+        // if(data.success){
+        // return
+        // }
+
+        return { data: data, error: null };
+      }
     } catch (error) {
       return { data: null, error: { message: "Something Went Wrong" } };
     }
